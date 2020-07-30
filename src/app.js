414c30da6fea568cb0fd58d195abfa909d94a604
Etienne Burdet <etienne.burdet@gmail.com>
2020-07-30 17:37:26 +0200

    get tracks data into app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import getOdsUrl from './plugins/odsql.js'
 
-const getDatasetUrl = getOdsUrl('examples')('world-heritage-unesco-list')
+const getRecords = getOdsUrl('eburdet')('etienne-tracks')
 
-const agregateUrl = getDatasetUrl({
-  select: 'count(*) as count',
-  where: 'category like "cultural"',
-  group_by: 'country'
-})
-
-fetch(agregateUrl).then(res => res.json())
+fetch(getRecords())
+  .then(res => res.json())
   .then(data => console.log(data))
--- src/plugins/odsql.js
+++ src/plugins/odsql.js
@@ -1,6 +1,7 @@
-export default baseDomain => datasetId => queryParams => {
+export default (baseDomain = 'data') => (datasetId = 'datasetid') => (queryParams = {}) => {
   const url = `https://${baseDomain}.opendatasoft.com/api/v2/catalog/datasets/${datasetId}/records?`
-  return Object.keys(queryParams).reduce(addQueryParamToUrl(queryParams), url)
+  const urlWithParams = Object.keys(queryParams).reduce(addQueryParamToUrl(queryParams), url)
+  return encodeURI(urlWithParams)
 }
 
 const addQueryParamToUrl = params => (url, key) => `${url}${key}=${params[key]}&`
