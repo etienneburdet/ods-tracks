521920a04bc4b3a22e7b3963674331b555bd1e97
Etienne Burdet <etienne.burdet@gmail.com>
2020-08-14 17:02:14 +0200

    build for deploy

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -1,10 +1,25 @@
 <script>
   import { fly } from 'svelte/transition'
+  import TopBar from './TopBar.svelte'
 </script>
 
 <div
   in:fly={ { x: 200, duration: 300, delay: 100} }
   out:fly={ { x: 200, duration: 300} }>
+  <TopBar>
+    <a href="#" on:click={trackId.reset}>
+      Sport
+    </a>
+    <a href={track.file.url}>
+      Difficulté
+    </a>
+    <a href={track.url}>
+      Durée
+    </a>
+    <a href="#">
+      Dénivelé
+    </a>
+  </TopBar>
   <slot></slot>
 </div>
 
