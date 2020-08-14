bb0a5e78bcf544e50caa1d316b149370bddf7ff1
Etienne Burdet <etienne.burdet@gmail.com>
2020-08-14 16:19:09 +0200

    top menu not centered

--- src/components/Details.svelte
+++ src/components/Details.svelte
@@ -2,6 +2,7 @@
   import { fly } from 'svelte/transition'
   import { trackId } from './store.js'
   import DetailsHeader from './DetailsHeader.svelte'
+  import TopBar from './TopBar.svelte'
 
   export let track
 
@@ -16,9 +17,24 @@
 <div
   in:fly={ { x: -200, duration: 300, delay: 100} }
   out:fly={ { x: -200, duration: 300} }>
-  <button on:click={trackId.reset}>X Retours liste des tracés</button>
-  <a href={track.file.url}>.gpx</a>
-  <a href={track.url}>more on strava</a>
+  <TopBar>
+    <a href="#" on:click={trackId.reset}>
+      <img src="arrow-back-outline.svg" alt="download">
+      Liste
+    </a>
+    <a href={track.file.url}>
+      <img src="download-outline.svg" alt="download">
+      .gpx
+    </a>
+    <a href={track.url}>
+      <img src="link-outline.svg" alt="external link">
+      Strava
+    </a>
+    <a href="#">
+      <img src="share-social-outline.svg" alt="external link">
+      Copier Url
+    </a>
+  </TopBar>
   <DetailsHeader
     {specs}
     image={track.image}
@@ -30,8 +46,28 @@
 <style lang="scss">
   div {
     position: relative;
-    background: white;
+    background: transparent;
     top: 50vh;
     width: 100vw;
+  }
+
+  img {
+    height: 1rem;
+    width: 1rem;
+    vertical-align: text-bottom;
+    margin-right: 5px;
+  }
+
+  a {
+    border-left: 1px solid #cbd2db;
+    &:first-child {
+      border: none;
+    }
+    padding: 3px 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: black;
   }
 </style>
---
+++ src/components/TopBar.svelte
@@ -0,0 +1,18 @@
+<div>
+  <slot></slot>
+</div>
+
+<style lang="scss">
+  div {
+    position: sticky;
+    top: 0;
+    display: inline-flex;
+    text-align: center;
+    padding: 13px 8px;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+    background: white;
+    box-shadow: 0 1px 2px 0 rgba(60,64,67,0.3),0 2px 6px 2px rgba(60,64,67,0.15);
+  }
+</style>
