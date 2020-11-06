f9d5cfaf54f937733168fed43edbc13a2f4b2f71
Etienne Burdet <etienne.burdet@gmail.com>
2020-11-06 10:31:05 +0100

    Add bottom button in menu

--- src/components/FiltersMenu.svelte
+++ src/components/FiltersMenu.svelte
@@ -1,15 +1,23 @@
 <script>
 import { slide } from 'svelte/transition'
+
+import { filteredTracks } from '../stores/filtered-tracks.js'
 </script>
 
 <div transition:slide>
   <h1>Filtres</h1>
   <slot></slot>
+  <footer>
+    <button type="button" name="close">
+      See {$filteredTracks.length} tracks
+    </button>
+  </footer>
 </div>
 
 <style>
 
   h1 {
+    flex: 0 0 auto;
     font-size: 21px;
     display: block;
     padding-top: 34px;
@@ -22,9 +30,22 @@ import { slide } from 'svelte/transition'
     top: 0;
     height: 100%;
     width: 100%;
+    display: flex;
+    flex-direction: column;
     background: white;
     z-index: 2;
     padding: 21px;
     box-sizing: border-box;
+  }
+
+  footer {
+    border-top: 1px solid rgb(235, 235, 235);
+  }
+
+  button {
+    position: absolute;
+    bottom: 0;
+    display: block;
+    width: 100%;
   }
 </style>
